2f91360d16105021e46e03efd7faeca76dc5d6d7
MySagarGithub <Pandaysagar2004@gmail.com>
2026-08-26 22:11:21 +0545

    update the app

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: "./src/setupTests.js",
+    setupFiles: path.resolve(__dirname, "src/setupTests.js"),
   },
 });
